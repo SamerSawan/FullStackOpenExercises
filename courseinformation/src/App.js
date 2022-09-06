2af287575cfe95933acb13272b0d17a9b829aee5
Samer Sawan <samersawan123@gmail.com>
2022-09-06 08:54:39 -0400

    finished exercises 2.4 and 2.5 of part 2 of FullStackOpen

--- courseinformation/src/App.js
+++ courseinformation/src/App.js
@@ -1,31 +1,56 @@
 import Course from "./components/Course.js"
 const App = () => {
-  const course = {
-    name: 'Half Stack Application development',
-    parts: [
-      {
-        id: 1,
-        name: "Fundamentals of React",
-        exercises: 10
-      },
-      {
-        id: 2,
-        name: "Using props to pass data",
-        exercises: 7
-      },
-      {
-        id: 3,
-        name: "State of a component",
-        exercises: 14
-      },
-      {
-        id: 4,
-        name: "Redux",
-        exercises: 11
-      }
-    ]
-  }
-  return <Course course={course}/>
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  return (
+    <div>
+      <h1>Web Development Curriculum</h1>
+      {courses.map(course => <Course key={course.id} course={course}/>)}
+    </div>
+    
+  )
 }
 
 export default App
